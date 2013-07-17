6638a61fb4cf2d0104a2fbe9ec6e0305da6c870e
inkerra <inkerra@gmail.com>
2013-07-17 04:35:54 -0400

    Add volume ACL support.
    blueprint volume-acl
    
    Change-Id: I1419c5ed0cbdc4d7d540912b9d6a35e8f1ae9f02

---
+++ cinderclient/v2/volume_acl.py
@@ -0,0 +1,95 @@
+# Copyright (C) 2013 Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Volume ACL permission interface (1.1 extension).
+"""
+
+from cinderclient import base
+
+
+class VolumeACLPermission(base.Resource):
+    """Volume ACL permission."""
+    def __repr__(self):
+        return "<VolumeACLPermission: %s>" % self.id
+
+    def delete(self):
+        """Delete this volume permission."""
+        return self.manager.delete(self)
+
+
+class VolumeACLPermissionManager(base.ManagerWithFind):
+    """Manage :class:`VolumeACLPermission` resources."""
+    resource_class = VolumeACLPermission
+
+    def create(self, volume_id, type, user_or_group_id, access_permission):
+        """Create a volume permission.
+
+        :param volume_id: The ID of the volume permission.
+        :param type: type of subject (user or group).
+        :param user_or_group_id: The ID of the user or the group.
+        :param access_permission: access permission number.
+        :rtype: :class:`VolumeACLPermission`
+        """
+        body = {'volume_acl_permission':
+                {'volume_id': volume_id,
+                 'type': type,
+                 'user_or_group_id': user_or_group_id,
+                 'access_permission': access_permission,
+                 }
+                }
+        return self._create('/os-volume-acl', body, 'volume_acl_permission')
+
+    def get(self, volume_permission_id):
+        """Show details of a volume permission.
+
+        :param volume_permission_id: The ID of the volume permission to display
+        :rtype: :class:`VolumeACLPermission`
+        """
+        return self._get("/os-volume-acl/%s" % volume_permission_id,
+                         "volume_acl_permission")
+
+    def get_all_by_volume(self, volume_id, detailed=True):
+        """Get a list of all volume permissions for a volume.
+
+        :param volume_id: The ID of the volume
+        :rtype: list of :class:`VolumeACLPermission`
+        """
+        if detailed is True:
+            return self._list("/os-volume-acl/detail?volume_id=%s" % volume_id,
+                              "volume_acl_permissions")
+        else:
+            return self._list("/os-volume-acl/?volume_id=%s" % volume_id,
+                              "volume_acl_permissions")
+
+    def list(self, detailed=True):
+        """Get a list of all volume permissions.
+
+        :rtype: list of :class:`VolumeACLPermission`
+        """
+        if detailed is True:
+            return self._list("/os-volume-acl/detail",
+                              "volume_acl_permissions")
+        else:
+            return self._list("/os-volume-acl",
+                              "volume_acl_permissions")
+
+    def delete(self, volume_permission_id):
+        """Delete a volume permission.
+
+        :param volume_permission_id: The :class:`VolumeACLPermission` to delete
+        """
+        self._delete("/os-volume-acl/%s" %
+                     base.getid(volume_permission_id))
